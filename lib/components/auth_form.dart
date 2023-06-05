3d00f351b10902842d87616a194ddb398b801e8d
Bruno Pivoto <brunorangel@gec.inatel.br>
2023-06-05 14:03:46 -0300

    select avatar

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -1,5 +1,8 @@
 // ignore_for_file: no_leading_underscores_for_local_identifiers
 
+import 'dart:io';
+
+import 'package:chat/components/user_image_picker.dart';
 import 'package:chat/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
@@ -15,10 +18,24 @@ class _AuthFormState extends State<AuthForm> {
   final _formKey = GlobalKey<FormState>();
   final _formData = AuthFormData();
 
+  void _handleImagePick(File image) {
+    _formData.image = image;
+  }
+
+  void _showError(String msg) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(msg),
+      backgroundColor: Theme.of(context).colorScheme.error,
+    ));
+  }
+
   void _submit() {
     final isValid = _formKey.currentState?.validate() ?? false;
 
     if (!isValid) return;
+    if (_formData.image == null && _formData.isSignup) {
+      return _showError('Imagem não selecionada');
+    }
     widget.onSubmit(_formData);
   }
 
@@ -32,6 +49,7 @@ class _AuthFormState extends State<AuthForm> {
             key: _formKey,
             child: Column(
               children: [
+                if (_formData.isSignup) UserImagePicker(onImagePick: _handleImagePick),
                 if (_formData.isSignup)
                   TextFormField(
                     initialValue: _formData.name,
