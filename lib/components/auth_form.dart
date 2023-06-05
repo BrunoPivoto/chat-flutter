02c538609c6f3a82ebfe8a884c4f8dfbffcefa7c
Bruno Pivoto <brunorangel@gec.inatel.br>
2023-06-05 13:13:34 -0300

    form onPress and validators

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -1,7 +1,26 @@
+// ignore_for_file: no_leading_underscores_for_local_identifiers
+
+import 'package:chat/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
-class AuthForm extends StatelessWidget {
+class AuthForm extends StatefulWidget {
   const AuthForm({super.key});
+
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _formData = AuthFormData();
+
+  void _submit() {
+    final isValid = _formKey.currentState?.validate() ?? false;
+
+    if (!isValid) return;
+      
+    
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,23 +29,59 @@ class AuthForm extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
+            key: _formKey,
             child: Column(
-          children: [
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Nome'),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Email'),
-            ),
-            TextFormField(
-              obscureText: true,
-              decoration: const InputDecoration(labelText: 'Senha'),
-            ),
-            const SizedBox(height: 12),
-            ElevatedButton(onPressed: () {}, child: const Text('Entrar')),
-            TextButton(onPressed: () {}, child: const Text('Criar nova conta'))
-          ],
-        )),
+              children: [
+                if (_formData.isSignup)
+                  TextFormField(
+                    initialValue: _formData.name,
+                    onChanged: (name) => _formData.name = name,
+                    key: const ValueKey('name'),
+                    decoration: const InputDecoration(labelText: 'Nome'),
+                    validator: (_name) {
+                      final name = _name ?? '';
+                      if (name.trim().length < 5) {
+                        return 'Nome deve ter mais de 5 caracteres';
+                      }
+                      return null;
+                    },
+                  ),
+                TextFormField(
+                  key: const ValueKey('email'),
+                  onChanged: (email) => _formData.email = email,
+                  decoration: const InputDecoration(labelText: 'Email'),
+                  validator: (_email) {
+                    final email = _email ?? '';
+                    if (!email.contains('@')) {
+                      return 'Email inválido';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  key: const ValueKey('password'),
+                  onChanged: (password) => _formData.password = password,
+                  obscureText: true,
+                  decoration: const InputDecoration(labelText: 'Senha'),
+                  validator: (_password) {
+                    final password = _password ?? '';
+                    if (password.length < 6) {
+                      return 'Senha deve ter no mínimo 6 caracteres';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 12),
+                ElevatedButton(onPressed: _submit, child: Text(_formData.islogin ? 'Entrar' : 'Cadastrar')),
+                TextButton(
+                    onPressed: () {
+                      setState(() {
+                        _formData.toggleAuthMode();
+                      });
+                    },
+                    child: Text(_formData.islogin ? 'Criar nova conta' : 'Já possui conta?'))
+              ],
+            )),
       ),
     );
   }
