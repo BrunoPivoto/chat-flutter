f54b60a16f31120d7d9adbe4f737939bb1f14bf1
Bruno Pivoto <brunorangel@gec.inatel.br>
2023-06-06 10:43:53 -0300

    auth methods, stream controller

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -1,33 +1,53 @@
 import 'dart:io';
+import 'dart:async';
+import 'dart:math';
 
 import 'package:chat/core/models/chat_user.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
 
 class AuthMockService implements AuthService {
-  @override
-  // TODO: implement currentUser
-  ChatUser? get currentUser => throw UnimplementedError();
+  static Map<String, ChatUser> _users = {};
+  static ChatUser? _currentUser;
+  static MultiStreamController<ChatUser?>? _controller;
+  static final _userStream = Stream<ChatUser?>.multi((controller) {
+    _controller = controller;
+    _updateUser(null);
+  });
 
   @override
-  Future<void> login(String email, String password) async {
-    // TODO: implement login
-    throw UnimplementedError();
+  ChatUser? get currentUser {
+    return _currentUser;
   }
 
   @override
-  Future<void> logout() async {
-    // TODO: implement logout
-    throw UnimplementedError();
+  Stream<ChatUser?> get userChanges {
+    return _userStream;
   }
 
   @override
   Future<void> signup(String name, String email, String password, File image) async {
-    // TODO: implement signup
-    throw UnimplementedError();
+    final newUser = ChatUser(
+      id: Random().nextDouble().toString(),
+      name: name,
+      email: email,
+      imageUrl: image.path,
+    );
+    _users.putIfAbsent(email, () => newUser);
+    _updateUser(newUser);
   }
 
   @override
-  // TODO: implement userChanges
-  Stream<ChatUser?> get userChanges => throw UnimplementedError();
-  
+  Future<void> login(String email, String password) async {
+    _updateUser(_users[email]);
+  }
+
+  @override
+  Future<void> logout() async {
+    _updateUser(null);
+  }
+
+  static void _updateUser(ChatUser? user) {
+    _currentUser = user;
+    _controller?.add(_currentUser);
+  }
 }
