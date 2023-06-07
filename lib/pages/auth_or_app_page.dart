e1e2b586087b9762ac813221eaeba7f55fa1c28d
Bruno Pivoto <brunorangel@gec.inatel.br>
2023-06-07 14:15:29 -0300

    login and signup firebase

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -1,4 +1,5 @@
-import 'package:chat/core/services/auth/auth_mock_service.dart';
+import 'package:chat/core/models/chat_user.dart';
+import 'package:chat/core/services/auth/auth_service.dart';
 import 'package:chat/pages/auth_page.dart';
 import 'package:chat/pages/chat_page.dart';
 import 'package:chat/pages/loading_page.dart';
@@ -20,8 +21,8 @@ class AuthOrAppPage extends StatelessWidget {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const LoadingPage();
         } else {
-          return StreamBuilder(
-            stream: AuthMockService().userChanges,
+          return StreamBuilder<ChatUser?>(
+            stream: AuthService().userChanges,
             builder: (ctx, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
                 return const LoadingPage();
